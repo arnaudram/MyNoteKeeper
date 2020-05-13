7cb37d692eb6ac3f2da9c065faf4d5e420668a46
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-05-13 04:57:42 -0700

    menu_share_note

--- app/src/main/java/com/example/mynotekeeper/fragment/DetailNoteFragment.kt
+++ app/src/main/java/com/example/mynotekeeper/fragment/DetailNoteFragment.kt
@@ -1,10 +1,10 @@
 package com.example.mynotekeeper.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.mynotekeeper.R
 import com.example.mynotekeeper.databinding.FragmentDetailNoteBinding
 
 class DetailNoteFragment:Fragment() {
@@ -17,6 +17,27 @@ class DetailNoteFragment:Fragment() {
          val receidNote=DetailNoteFragmentArgs.fromBundle(requireArguments()).itemNote
         binding.noteSelected=receidNote
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_share_note,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.share_note-> {
+                shareNote()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun shareNote() {
+      Toast.makeText(context,"Share With",Toast.LENGTH_LONG).show()
     }
 }
