1dd1c17d7a8299741a21fdaeb56e3c8fa1baf985
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-05-13 06:24:16 -0700

    share_option

--- app/src/main/java/com/example/mynotekeeper/fragment/DetailNoteFragment.kt
+++ app/src/main/java/com/example/mynotekeeper/fragment/DetailNoteFragment.kt
@@ -1,20 +1,24 @@
 package com.example.mynotekeeper.fragment
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.mynotekeeper.R
 import com.example.mynotekeeper.databinding.FragmentDetailNoteBinding
+import com.example.mynotekeeper.dataclasses.Note
 
 class DetailNoteFragment:Fragment() {
+    lateinit var receidNote:Note
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding=FragmentDetailNoteBinding.inflate(inflater)
-         val receidNote=DetailNoteFragmentArgs.fromBundle(requireArguments()).itemNote
+          receidNote=DetailNoteFragmentArgs.fromBundle(requireArguments()).itemNote
         binding.noteSelected=receidNote
 
         setHasOptionsMenu(true)
@@ -38,6 +42,18 @@ class DetailNoteFragment:Fragment() {
     }
 
     private fun shareNote() {
-      Toast.makeText(context,"Share With",Toast.LENGTH_LONG).show()
+     // Toast.makeText(context,"Share With",Toast.LENGTH_LONG).show()
+        val subject="${receidNote.note_title}"
+        val text="hello sir, this is what i have learnt so for. ${receidNote.note_course} \n ${receidNote.note_title} \n ${receidNote.note_text}"
+        val shareNoteIntent=Intent(Intent.ACTION_SEND)
+            .setType("message/rfc2822")
+            .putExtra("subject",subject)
+            .putExtra("text",text)
+
+      val chooser=Intent.createChooser(shareNoteIntent,"Share With")
+       // startActivity(shareNoteIntent)
+        if (shareNoteIntent.resolveActivity(requireActivity().packageManager)!=null){
+              startActivity(shareNoteIntent)
+        }
     }
 }
