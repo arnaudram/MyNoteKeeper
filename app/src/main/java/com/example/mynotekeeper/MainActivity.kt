7b2f93928267962cbde31dcf254f3dc576c12f54
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-05-16 10:40:29 -0700

    setting up navigation drawer

--- app/src/main/java/com/example/mynotekeeper/MainActivity.kt
+++ app/src/main/java/com/example/mynotekeeper/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.mynotekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.mynotekeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var binding: ActivityMainBinding
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+         binding=DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        drawerLayout=binding.navigationDrawer
+        val navigationView=binding.navigationView
+        val navController=findNavController(R.id.fragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navigationView,navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (controller.graph.startDestination==destination.id){
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }
+            else{
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=findNavController(R.id.fragment)
+        return  NavigationUI.navigateUp(navController,drawerLayout) ||super.onSupportNavigateUp()
     }
 }
